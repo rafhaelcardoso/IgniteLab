953af107803b0bfcfbbda047ef2a69d3c83f4d70
rafhaelcardoso <rafhaelcardosos@gmail.com>
2022-06-25 16:36:07 -0300

    feat: :sparkles: Create Header component
    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Logo } from './Logo'
+
+export function Header() {
+    return (
+        <header className='w-full py-5 flex items-center justify-center bg-gray-700 border-b-1 border-gray-600'>
+            <Logo />
+        </header>
+    )
+}
